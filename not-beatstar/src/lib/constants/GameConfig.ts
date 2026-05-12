3e6911a80fd2cf472852403c16741bb6be5a9618
Sam Akiode <sam.akiode03@gmail.com>
2026-05-12 16:15:57 +0100

    Changed config to introduce PERFECT+ scoring (will need refactor of existing structure)

--- not-beatstar/src/lib/constants/GameConfig.ts
+++ not-beatstar/src/lib/constants/GameConfig.ts
@@ -2,6 +2,7 @@ export const AUDIO_LATENCY_MS: number = 380;
 
 export const HIT_WINDOW_MS: number = 250;
 export const PERFECT_WINDOW_MS: number = 80;
+export const PERFECT_PLUS_WINDOW_MS: number = 45;
 
 export const TRAVEL_TIME_MS: number = 2000;
 export const THRESHOLD_OFFSET_PERCENT: number = 0.18;
