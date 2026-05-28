05f84d670571946efd6d5cca85bea17e098bf2a1
Sam Akiode <sam.akiode03@gmail.com>
2026-05-28 15:35:29 +0100

    sections for new song

---
+++ not-beatstar/src/lib/test/testSong2.ts
@@ -0,0 +1,29 @@
+import type { Song } from "../interfaces";
+
+export const testSong: Song = {
+  "id": "",
+  "title": "Nothing Matters",
+  "artist": "The Last Dinner Party",
+  "uri": "/audio/the-last-dinner-party_nothing-matters.mp3",
+  "sections": [
+    {
+      "id": 0,
+      "startTimeMs": 0,
+      "endTimeMs": 23092,
+      "description": "verse"
+    },
+    {
+      "id": 1,
+      "startTimeMs": 23092,
+      "endTimeMs": 32519,
+      "description": "bridge"
+    },
+    {
+      "id": 2,
+      "startTimeMs": 32519,
+      "endTimeMs": 54360,
+      "description": "chorus"
+    }
+  ],
+  "pattern": []
+}
